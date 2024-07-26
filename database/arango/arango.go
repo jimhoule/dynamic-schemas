d987f200df5111c20e4e368b4339cf23a9ab84f4
Jimmy <jimhoule09@gmail.com>
2024-07-26 18:34:57 -0400

    arango database driver adjustments

--- database/arango/arango.go
+++ database/arango/arango.go
@@ -10,8 +10,29 @@ import (
 	"github.com/arangodb/go-driver/http"
 )
 
+// validation schema types
+type PropertyArrayItem struct {
+	Type    string `json:"type"`
+	Maximum int    `json:"maximum,omitempty"`
+}
+type Property struct {
+	Type  string            `json:"type"`
+	Items PropertyArrayItem `json:"items,omitempty"`
+}
+type Rule struct {
+	Properties map[string]Property `json:"properties"`
+	Required   []string            `json:"required"`
+}
+type Properties struct {
+	Rule  Rule   `json:"rule"`
+	Level string `json:"level"`
+}
+
 type DriverDatabase = driver.Database
 type DriverClient = driver.Client
+type DriverCollection = driver.Collection
+type CreateCollectionOptions = driver.CreateCollectionOptions
+type CreateCollectionPropertiesOptions = driver.CollectionSchemaOptions
 
 type ArangoDb[TDatabase DriverDatabase, TClient DriverClient] struct{}
 

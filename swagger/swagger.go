cdf3260856b4d4b4f88a1239c88a177104103431
Jimmy Houle <jimhoule09@gmail.com>
2024-07-29 10:24:10 -0400

    putting all openapi specs into one file sp it can be coupled with swagger ui

---
+++ swagger/swagger.go
@@ -0,0 +1,18 @@
+package swagger
+
+import (
+	"embed"
+	"io/fs"
+	"main/router"
+	"net/http"
+)
+
+//go:embed ui
+var embeddedUi embed.FS
+
+func Init(mainRouter *router.MainRouter) {
+	sub, _ := fs.Sub(embeddedUi, "ui")
+	fs := http.FileServer(http.FS(sub))
+	
+	mainRouter.Handle("/doc", http.StripPrefix("/doc/", fs))
+}

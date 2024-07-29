97cf5f1c3ae6116a43774ef2441a74aae2789b11
Jimmy <jimhoule09@gmail.com>
2024-07-29 15:57:52 -0400

    creation of specs module that contains open api specs and swagger ui embedded template

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"main/documents"
 	"main/router"
 	"main/schemas"
-	"main/swagger"
+	"main/specs"
 	"main/tenants"
 	"net/http"
 	"os"
@@ -45,7 +45,7 @@ func main() {
 	schemas.Init(mainRouter, appDbHandler)
 	collections.Init(mainRouter, appDbHandler)
 	documents.Init(mainRouter, appDbHandler)
-	swagger.Init(mainRouter)
+	specs.Init(mainRouter)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf("%s:%s", os.Getenv("HTTP_URL"), os.Getenv("HTTP_PORT")),
